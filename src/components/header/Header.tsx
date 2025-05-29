3f9356880d05c536f0b4a0ec3c3f57b4f8c85a01
d1sit3 <leshachernov200@mail.ru>
2025-05-29 22:57:31 +1000

    sec comm

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,14 @@
+import '../../styles/header.scss'
+import User from '../../img/user-fill.svg'
+import Basket from '../../img/shopping-basket-fill.svg'
 export default function Header(){
     return(
         <header className='header'>
             <h1 className='header__logo'>SneakerHub</h1>
+            <div className='header__media'>
+                <img className='media__basket' src={Basket} alt="basket-logo" />
+                <img className='media__user' src={User} alt="user-logo"/>
+            </div>
         </header>
     )
 }
