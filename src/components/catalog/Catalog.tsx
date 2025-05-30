6b6c22d9c95dfd842294ffa9df08492b94efe46c
d1sit3 <leshachernov200@mail.ru>
2025-05-30 23:10:12 +1000

    fourth comm

--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -1,10 +1,31 @@
 import '../../styles/Catalog.scss'
+import { useCatalogStore } from '../../store/store'
+import { useEffect } from 'react';
 
 
 export default function Catalog(){
+    // constants
+    const products = useCatalogStore(state => state.products);
+    const fetchProducts = useCatalogStore(state => state.fetchProducts)
+
+
+    useEffect(() => {
+        try{
+            fetchProducts()
+        } catch(error){
+            console.log('Fatal error:', error)
+        }
+    }, [fetchProducts])
+    
+    
     return(
-        <section className='catalog'>
+        <div className='catalog'>
             <h2 className='catalog__subtitle'>Catalog</h2>
-        </section>
+            <div className='catalog__product'>
+                {products.map((product) =>(
+                    <div key={product.id}>{product.img} {product.title}. {product.cost}</div>
+                ))}
+            </div>
+        </div>
     )
 }
---
+++ src/store/store.ts
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+
+interface Product {
+    id: number,
+    title: string,
+    cost: string
+}
+
+interface ProductsState{
+    products: Product[],
+    isLoading: boolean,
+    errors: string[]
+    fetchProducts: () => void,
+}
+
+
+export const useCatalogStore = create<ProductsState>()((set) => ({
+    products: [],
+    isLoading: false,
+    errors: [],
+    fetchProducts: async () => {
+        const response = await fetch('http://localhost:3000/products')
+        const json = await response.json() as Product[];
+        set({products: json})
+    }
+}))
